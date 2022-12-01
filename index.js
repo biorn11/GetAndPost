22255a2f55e30c81bb05f79e35b2aee5dfa7ad43
Argen96 <106479453+Argen96@users.noreply.github.com>
2022-12-01 12:51:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ app.use(express.json())
 
 app.get('/sum', async (request,response)=>{
     
-    const sumAll = request.query.sum.split('').reduce((acc,el)=> acc+Number(el),0)
-    response.send(`The sum of the numbers [${request.query.sum}] is ${sumAll}`)
+    const sumAll = request.query.numbers.split('').reduce((acc,el)=> acc+Number(el),0)
+    response.send(`The sum of the numbers [${request.query.numbers}] is ${sumAll}`)
 })
 
 app.get('/unique',async(request,response)=>{
     
-    const uniqueValues = request.query.unique.split('').filter((el,i,arr)=> arr.indexOf(el)===i)
-    response.send(`The unique values in [${request.query.unique}] is ${uniqueValues}`)
+    const uniqueValues = request.query.numbers.split('').filter((el,i,arr)=> arr.indexOf(el)===i)
+    response.send(`The unique values in [${request.query.numbers}] is ${uniqueValues}`)
 })
 app.post('/sum',async(request,response)=>{
-    
-    const sumAll = request.query.sum.split('').reduce((acc,el)=> acc+Number(el),0)
-    response.send(`The sum of the numbers [${request.query.sum}] is ${sumAll}`)
+
+    const sumAll = request.query.numbers.split('').reduce((acc,el)=> acc+Number(el),0)
+    response.send(`The sum of the numbers [${request.query.numbers}] is ${sumAll}`)
 
 })
 
 app.post('/unique',async(request,response)=>{
     
-    const uniqueValues = request.query.unique.split('').filter((el,i,arr)=> arr.indexOf(el)===i)
-    response.send(`The unique values in [${request.query.unique}] are ${uniqueValues}`)
+    const uniqueValues = request.query.numbers.split('').filter((el,i,arr)=> arr.indexOf(el)===i)
+    response.send(`The unique values in [${request.query.numbers}] are ${uniqueValues}`)
 })
 
 const port = 3000
